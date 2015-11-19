0a87a6f9d0e80e57dea978a68c7a2b4b3777b406
daniel-meier <dm077@hdm-stuttgart.de>
2015-11-19 17:38:32 +0100

    Attribute und Methoden angelegt

--- src/de/hdm/itprojekt/messenger/shared/bo/Abonnement.java
+++ src/de/hdm/itprojekt/messenger/shared/bo/Abonnement.java
@@ -1,5 +1,25 @@
 package de.hdm.itprojekt.messenger.shared.bo;
 
-public class Abonnement {
+public class Abonnement extends BusinessObject {
 
+	private static final long serialVersionUID = 1L;
+
+	private Nutzer abonnent = null;
+	private Abonnement abonnement = null;
+
+	public Abonnement () {
+	}
+	
+	public Nutzer getAbonnent() {
+		return abonnent;
+	}
+	
+	public Abonnement getAbonnement() {
+		return abonnement;
+	}
+
+	public void setAbonnement(Abonnement abonnement) {
+		this.abonnement  = abonnement;
+	}
+	
 }
