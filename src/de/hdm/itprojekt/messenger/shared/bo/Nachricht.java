a784bdf2a98afa31f1b18fdadd5e2e774d91b322
KimIshola <ki004@hdm-stuttgart.de>
2015-11-19 14:24:48 +0100

    Attribute und Methoden angelegt

--- src/de/hdm/itprojekt/messenger/shared/bo/Nachricht.java
+++ src/de/hdm/itprojekt/messenger/shared/bo/Nachricht.java
@@ -1,5 +1,42 @@
 package de.hdm.itprojekt.messenger.shared.bo;
 
+import java.util.ArrayList;
+
 public class Nachricht {
+	
+	
+	//Anlegen der Attribute
+	private static final long serialVersionUID =1L;
+	private String text;
+	private Nutzer sender;
+	private ArrayList<Nutzer> empfeanger;
+	private Nutzer empfaenger;
+	
+	
+	//Anlegen der Methoden 
+	
+	public Nachricht() {
+	}
+	
+	public String getText(){
+		return text;
+	}
+	
+	public Nutzer getSender(){
+		return sender;
+	}
+	
+	public Nutzer getEmpfaenger(){
+		return empfaenger;
+	}
+	
+	public void setText(String text){
+		
+	}
+	
+	public void setEmpfaenger (ArrayList<Nutzer> enpfanger){
+		
+	}
+	
 
 }
