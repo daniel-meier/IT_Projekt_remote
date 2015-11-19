659d653797ac6081db6ba4277a3c9d6c3d8d2903
Sarah Weimer <sw155@hdm-stuttgart.de>
2015-11-19 17:47:07 +0100

    Methodenköpfe der Klasse NutzerMapper hinzugefügt.

--- src/de/hdm/itprojekt/messenger/server/db/NutzerMapper.java
+++ src/de/hdm/itprojekt/messenger/server/db/NutzerMapper.java
@@ -1,5 +1,64 @@
 package de.hdm.itprojekt.messenger.server.db;
 
 public class NutzerMapper {
+	
+	public NutzerMapper () {
+		
+	}
+	
+	public NutzerMapper nutzerMapper() {
+		
+	}
+	
+	/** Suche eines Nutzers nach seiner eindeutigen Nummer
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public NutzerMapper findByNutzerID (int id) {
+		
+	}
+	
+	/** Suche ein Nutzer Objekt nach seinem Nachnamen
+	 * 
+	 * @return
+	 */
+	public Vector<Nutzer> findByNachname () {
+		
+	}
+	
+	/** Einfügen eines Nutzer Objekts in die Datenbank
+	 * 
+	 * @param NutzerMapper
+	 * @return
+	 */
+	public NutzerMapper einfuegen (nutzerMapper NutzerMapper) {
+		
+	}
+	
+	/** Bearbeiten eines Nutzer Objekts in der Datenbank
+	 * 
+	 * @param NutzerMapper
+	 * @return
+	 */
+	public NutzerMapper bearbeiten (nutzerMapper NutzerMapper) {
+		
+	}
+	
+	/** Löschen eines Nutzer Objekts aus der Datenbank
+	 * 
+	 * @param NutzerMapper
+	 */
+	public void loeschen (nutzerMapper NutzerMapper) {
+		
+	}
+	
+	/** Auslesen aller Nutzer
+	 * 
+	 * @return
+	 */
+	public Vector<Nutzer> getNutzer() {
+		
+	}
 
 }
