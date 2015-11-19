fe4c3805edaabbea7b2a40f07e218aaefaa2b466
ChristianReichardt <cr076@hdm-stuttgart.de>
2015-11-19 16:35:47 +0100

    Attribute und Methoden Klasser Hashtag

--- src/de/hdm/itprojekt/messenger/shared/bo/Hashtag.java
+++ src/de/hdm/itprojekt/messenger/shared/bo/Hashtag.java
@@ -1,5 +1,19 @@
 package de.hdm.itprojekt.messenger.shared.bo;
 
 public class Hashtag {
+	
+	private String name;
+	
+	public Hashtag(){
+		
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public void setName (String name){
+		this.name = name;
+	}
 
 }
