074fe89e2abebaaeea17e9fb507f498321abfeb4
Sarah Weimer <sw155@hdm-stuttgart.de>
2015-11-19 14:32:14 +0100

    Attribute und Methoden von den Klassen HashtagAbonnement, NutzerAbonnement und Nutzer erstellt.

--- src/de/hdm/itprojekt/messenger/shared/bo/NutzerAbonnement.java
+++ src/de/hdm/itprojekt/messenger/shared/bo/NutzerAbonnement.java
@@ -1,5 +1,15 @@
 package de.hdm.itprojekt.messenger.shared.bo;
 
 public class NutzerAbonnement {
+	
+	private int abonnierterNutzerID;
+	
+	public int getAbonnierterNutzerID () {
+		return abonnierterNutzerID;
+	}
+	
+	public void setAbonnierterNutzerID (int id) {
+		this.abonnierterNutzerID = abonnierterNutzerID;
+	}
 
 }
