4c1a139ffae874fe9fb1e37336587c62b2114e88
Sarah Weimer <sw155@hdm-stuttgart.de>
2015-11-19 15:23:27 +0100

    Methoden in die AbonnementMapper Klasse eingefügt.

--- src/de/hdm/itprojekt/messenger/server/db/AbonnementMapper.java
+++ src/de/hdm/itprojekt/messenger/server/db/AbonnementMapper.java
@@ -2,4 +2,71 @@ package de.hdm.itprojekt.messenger.server.db;
 
 public class AbonnementMapper {
 
+	public AbonnementMapper () {
+		
+	}
+	
+	public AbonnementMapper abonnementMapper () {
+		
+ 	}
+	
+	/** Suche eines Abonnements nach seiner eindeutigen Nummer
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public Abonnement findbyID (int id) {
+		
+	}
+	
+	/** Einfügen eines Abonnement Objekts in die Datenbank
+	 * 
+	 * @param Abonnement
+	 * @return
+	 */
+	public Abonnement einfuegen (abonnement Abonnement) {
+		
+	}
+	
+	/** Bearbeiten eines Abonnement Objekts in der Datenbank
+	 * 
+	 * @param Abonnement
+	 * @return
+	 */
+	public Abonnement bearbeiten (abonnement Abonnement) {
+		
+	}
+	
+	/** Löschen eines Abonnement Objekts aus der Datenbank.
+	 * 
+	 * @param Abonnement
+	 */
+	public void löschen (abonnement Abonnement) {
+		
+	}
+	
+	/** Auslesen aller Nutzer Abonnements
+	 * 
+	 * @return
+	 */
+	public Vector<Nutzer> getNutzerAbo (nutzer Nutzer) {
+		
+	}
+	
+	/** Auslesen aller Hashtag Abonnements
+	 * 
+	 * @return
+	 */
+	public Vector<Hashtag> getHashtagAbo (hashtag Hashtag) {
+		
+	}
+	
+	/** Auslesen aller Abonnements
+	 * 
+	 * @return
+	 */
+	public Vector<Abonnement> getAllAbo () {
+		
+	}
+	
 }
