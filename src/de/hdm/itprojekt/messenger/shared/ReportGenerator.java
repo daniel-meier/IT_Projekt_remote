457882a785d1603f4745c6df58889d8f3e5f3cd9
daniel-meier <dm077@hdm-stuttgart.de>
2015-11-20 23:16:43 +0100

    Methoden angelegt

--- src/de/hdm/itprojekt/messenger/shared/ReportGenerator.java
+++ src/de/hdm/itprojekt/messenger/shared/ReportGenerator.java
@@ -1,5 +1,51 @@
 package de.hdm.itprojekt.messenger.shared;
 
-public class ReportGenerator {
+import java.util.Date;
 
+import com.google.gwt.user.client.rpc.RemoteService;
+
+import de.hdm.itprojekt.messenger.shared.bo.Hashtag;
+import de.hdm.itprojekt.messenger.shared.bo.Nutzer;
+
+
+public interface ReportGenerator extends RemoteService {
+	
+	//Initialisierung des Objekts
+	public void init();
+	
+	//Setzen des zugeordneten Nutzer
+	public void setNutzer (Nutzer nutzer);
+	
+	//Erstellen eines NachrichtByNutzerReport-Reports.
+	//Dieser Report-Typ stellt sämtliche Nachrichten nach Nutzer dar.
+	public NachrichtByNutzerReport erstelleNachrichtByNutzerReport(Nutzer nutzer);
+	
+	//Erstellen eines NachrichtByNutzerZeitraumReport-Reports.
+	//Dieser Report-Typ stellt sämtliche Nachrichten nach Nutzer in einem bestimmten Zeitraum dar.
+	public NachrichtByNutzerZeitraumReport erstelleNachrichtByNutzerZeitraumReport(Nutzer nutzer, Date date);
+	
+	//Erstellen eines AbonnementByHashtagReport-Reports.
+	//Dieser Report-Typ stellt sämtliche Abonnements eines bestimmten Hashtags dar.
+	public AbonnementByHashtagReport erstelleAbonnementByHashtagReport(Hashtag hashtag);
+	
+	//Erstellen eines AlleHashtagAbonemmentReport-Reports.
+	//Dieser Report-Typ stellt sämtliche Hashtag Abonnements aller Nutzer dar.
+	public AllHashtagAbonnementReport erstelleAlleHashtagAbonemmentReport();
+	
+	//Erstellen eines AbonnementByNutzerReport-Reports.
+	//Dieser Report-Typ stellt sämtliche Abonnements von Nutzern eines Nutzers dar.
+	public AbonnementsByNutzerReport erstelleAbonnementByNutzerReport(Nutzer nutzer);
+	
+	//Erstellen eines AbonnementsByNutzerHashtagReport-Reports.
+	//???
+	public AbonnementsByNutzerHashtagReport erstelleAbonnementsByNutzerHashtagReport(Nutzer nutzer, Hashtag hashtag);
+	
+	//Erstellen eines NachrichtByZeitraumReport-Reports.
+	//Dieser Report-Typ stellt sämtliche Nachrichten in einem bestimmten Zeitraum dar.
+	public NachrichtByZeitraumReport erstelleNachrichtByZeitraumReport(Date date);
+	
+	//Erstellen eines AlleNutzerAbonnementsReport-Reports.
+	//Dieser Report-Typ stellt sämtliche Nutzer Abonnements aller Nutzer dar.
+	public AllNutzerAbonnementsReport erstelleAlleNutzerAbonnementsReport();
+	
 }
