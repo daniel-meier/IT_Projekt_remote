e5239b1dc65c62fc417c109c2e4402e4f4685b2a
KimIshola <ki004@hdm-stuttgart.de>
2015-11-22 14:06:12 +0100

    Kommentare Hinzugefügt

--- src/de/hdm/itprojekt/messenger/shared/bo/BusinessObject.java
+++ src/de/hdm/itprojekt/messenger/shared/bo/BusinessObject.java
@@ -4,32 +4,59 @@ import java.util.Date;
 
 public abstract class BusinessObject implements Serializable {
 	
+	/**
+	 * Anlegen der Attribute
+	 */
 	private static final long serialVersionUID = 1L;
 	private int id = 0;
 	private Date erstellungszeitpunkt;
 	
+	/**
+	 * Leerer Konstruktor
+	 */
 	public BusinessObject(){
 		
 	}
 	
+	/**
+	 * Auslesen der ID
+	 * @return id
+	 */
 	public int getID(){
 		return this.id;
 	}
 	
+	/**
+	 * Setzen der ID
+	 * @param id
+	 */
 	public void setID(int id){
 		this.id = id;
 	}
 	
+	/**
+	 * Auslesen des Erstellungszeitpunktes
+	 * @return erstellungszeitpunkt
+	 */
 	public Date getErstellungszeitpunkt(){
 		return this.erstellungszeitpunkt;
 	}
 	
+	/**
+	 * Erzeugen einer einfachen textuellen Darstellung der jeweiligen Instanz
+	 */
 	public String toString() {
+		/**
+		 * Wir geben den Klassennamen gefolgt von der ID des Objekts zurück
+		 */
 		return this.getClass().getName() + " #" + this.id;
 	  }
 	
+	
 	public boolean equals (Object o){
-		
+	/**
+	 * Abfragen, ob das Objekt gleich ist und ob ein Objekt gecastet werden kann	
+	 */
 		return false;
 	}
 		
