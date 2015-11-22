f0bb9c7f5387459a870ef93f0e64445e8dfdb210
KimIshola <ki004@hdm-stuttgart.de>
2015-11-22 13:43:31 +0100

    Kommentierung hinzugefügt

--- src/de/hdm/itprojekt/messenger/shared/bo/Abonnement.java
+++ src/de/hdm/itprojekt/messenger/shared/bo/Abonnement.java
@@ -1,19 +1,47 @@
 package de.hdm.itprojekt.messenger.shared.bo;
 
+/** 
+ * BusinessObject ist die Superklasse von Abonnement
+ * @author kimbo
+ *
+ */
 public class Abonnement extends BusinessObject {
-
+	
+	
+/**
+ * 
+ */
 	private static final long serialVersionUID = 1L;
-
+	
+	/**
+	 * Nutzer ist Abonnent
+	 */
 	private Nutzer abonnent = null;
+	
+	/**
+	 * Abonnement ist Abonnement 
+	 */
 	private Abonnement abonnement = null;
 
+	/**
+	 * Leerer Konstruktor
+	 */
 	public Abonnement () {
 	}
 	
+	/**
+	 * Auslesen der Abonnenten
+	 * @return abonnent
+	 */
 	public Nutzer getAbonnent() {
 		return abonnent;
 	}
 	
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public Abonnement getAbonnement() {
 		return abonnement;
 	}
