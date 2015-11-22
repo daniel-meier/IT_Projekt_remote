a800114b09f9cc83c81676134c5d9ed57ccb8708
tinohauler <th098@hdm-stuttgart.de>
2015-11-22 14:14:20 +0100

    Doppelte Methoden gelöscht in DBConnection

--- src/de/hdm/itprojekt/messenger/server/db/DBConnection.java
+++ src/de/hdm/itprojekt/messenger/server/db/DBConnection.java
@@ -12,25 +12,6 @@ import com.google.cloud.sql.jdbc.ResultSet;
 
 public class DBConnection {
 	
-	private static Connection con = null;
-	
-	/** 
-	 * Die Url mit deren Hilfe die Datenbank angesprochen wird.
-	 */
-	private static String url = //'jdbc.mysql://127.2.3 ...'
-			
-	
-	public static DBConnection() {
-		
-	}
-	
-	/**
-	 * 
-	 * @return Das DBConnection Objekt
-	 */
-	public static Connection connection () {
-		
-	}
 	
 
 	/**
