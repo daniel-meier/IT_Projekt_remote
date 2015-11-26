cc8edd2ef4690509c757f1c64bc213b5c50403ad
KimIshola <ki004@hdm-stuttgart.de>
2015-11-26 16:08:29 +0100

    Aktualisiert

--- src/de/hdm/itprojekt/messenger/client/Messenger.java
+++ src/de/hdm/itprojekt/messenger/client/Messenger.java
@@ -1,5 +1,17 @@
 package de.hdm.itprojekt.messenger.client;
 
-public class Messenger {
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+
+public class Messenger implements EntryPoint{
+	
+	
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		
+		HorizontalPanel hPanel = new HorizontalPanel();
+	}
 
 }
