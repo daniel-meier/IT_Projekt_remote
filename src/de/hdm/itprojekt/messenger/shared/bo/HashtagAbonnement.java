47ab387df8ba201a7b97391299aad162642941e0
Sarah Weimer <sw155@hdm-stuttgart.de>
2015-11-29 15:04:00 +0100

    Attribut hashtagAbonnement + getHashtagAbonnement, setHashtagAbonnement in der Klasse HashtagAbonnement hinzugefügt.

--- src/de/hdm/itprojekt/messenger/shared/bo/HashtagAbonnement.java
+++ src/de/hdm/itprojekt/messenger/shared/bo/HashtagAbonnement.java
@@ -1,22 +1,56 @@
 package de.hdm.itprojekt.messenger.shared.bo;
 
+/**
+ * Klasse Abonnement ist die Superklasse von HashtagAbonnement
+ * @author Sarah Weimer
+ *
+ */
 public class HashtagAbonnement extends Abonnement{
 	
+	/**
+	 * Attribute eines Hashtag Abonnements
+	 */
 	private static final long serialVersionUID = 1L;
 	private int abonnierterHashtagID;
+	private Abonnement hashtagAbonnement;
 
-
+	/**
+	 * Konstruktor
+	 */
 	public HashtagAbonnement () {
 	
 	}
 	
+	/**
+	 * Auslesen der abonnierten Hashtag ID
+	 * @return abonnierterHashtagID
+	 */
 	public int getAbonnierterHashtagID () {
 		return abonnierterHashtagID;
 	}
 	
+	/**
+	 * Setzen der abonnierten Hashtag ID
+	 * @param abonnierterHashtagID
+	 */
 	public void setAbonnierterHashtagID (int abonnierterHashtagID) {
 		this.abonnierterHashtagID = abonnierterHashtagID;
 	}
+	
+	/**
+	 * Auslesen der HashtagAbonnements 
+	 * @return
+	 */
+	public Abonnement getHashtagAbonnement () {
+		return hashtagAbonnement;
+	}
 
+	/**
+	 * Setzen der HashtagAbonnements
+	 * @param hashtagAbonnement
+	 */
+	public void setHashtagAbonnement(Abonnement hashtagAbonnement) {
+		this.hashtagAbonnement = hashtagAbonnement;
+	}
 
 }
