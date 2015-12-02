f5350bdd6223c1417a1db3395f33367789ea17fb
ChristianReichardt <cr076@hdm-stuttgart.de>
2015-12-02 16:44:31 +0100

    Neue Labels angelegt

--- src/de/hdm/itprojekt/messenger/client/Impressum.java
+++ src/de/hdm/itprojekt/messenger/client/Impressum.java
@@ -1,14 +1,27 @@
 package de.hdm.itprojekt.messenger.client;
 
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class Impressum extends VerticalPanel {
 	
 	public void onLoad() {
 		super.onLoad();
+		
+        HorizontalPanel headlinePanel = new HorizontalPanel();
+        RootPanel.get("headline").add(headlinePanel);
+        
 		Label l = new Label("Impressum");
-        this.add(l);
+        headlinePanel.add(l);
+        
+        Label lbl = new Label("Name1");
+        this.add(lbl);
+        
+        
+        
+        
 	}
 
 }
