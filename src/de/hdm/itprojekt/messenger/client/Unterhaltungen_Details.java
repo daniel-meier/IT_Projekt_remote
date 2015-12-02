060a29ead7f60d1addd5ff75a7cd6133ede9de0b
daniel-meier <dm077@hdm-stuttgart.de>
2015-12-02 12:53:59 +0100

    Headline div und panel hinzugefügt. textarea hinzugefügt button hinzugefügt. anordnung verändert. stylesheet angepasst.

--- src/de/hdm/itprojekt/messenger/client/Unterhaltungen_Details.java
+++ src/de/hdm/itprojekt/messenger/client/Unterhaltungen_Details.java
@@ -9,11 +9,13 @@ public class Unterhaltungen_Details extends VerticalPanel {
 	public void onLoad(){
 		
 		super.onLoad();
-		RichTextArea textArea = new RichTextArea();
-		this.add(textArea);
 		
 		
-		
+		/**
+		 * Horizontal Panel für Überschrift
+		 */
+	    HorizontalPanel headlinePanel = new HorizontalPanel();
+		RootPanel.get("headline").add(headlinePanel);
 		
 		/**
 		 * Horizontal Panel für Edit-Buttons
@@ -21,7 +23,27 @@ public class Unterhaltungen_Details extends VerticalPanel {
 	    HorizontalPanel buttonPanel = new HorizontalPanel();
 		RootPanel.get("editbuttons").add(buttonPanel);
 	    
-	    
+		
+		
+		/**
+		 * Label für Beschriftung der folgenden Textbox. 
+		 * Text des Labels: "Unterhaltung mit"
+		 */
+	    Label l = new Label("Unterhaltung mit ");
+		l.setStylePrimaryName("label1");
+		headlinePanel.add(l);
+		
+		/**
+		 * Read-only Textbox für Ausgabe der Teilnehmernamen
+		 */
+		TextBox teilnehmerNamen = new TextBox();
+		teilnehmerNamen.setText("platzhaltername");
+		teilnehmerNamen.setEnabled(false);
+		teilnehmerNamen.setStylePrimaryName("teilnehmerNamen");
+	    headlinePanel.add(teilnehmerNamen);
+
+		
+		
 	    /**
 		 * Button
 		 */
@@ -51,6 +73,26 @@ public class Unterhaltungen_Details extends VerticalPanel {
 		final Button zurückButton = new Button("zurück");
 		zurückButton.setStylePrimaryName("edit-button");
 		buttonPanel.add(zurückButton);
+		
+		
+		/**
+		 * Ausgabe des Chats
+		 */
+		RichTextArea textAreaAusgabe = new RichTextArea();
+		this.add(textAreaAusgabe);
+		
+		/**
+		 * Eingabe der neuen zu sendenden Nachricht/Antwort
+		 */
+		RichTextArea textAreaEingabe = new RichTextArea();
+		this.add(textAreaEingabe);
+		
+		/**
+		 * Button zum Absenden der Nachricht/Antwort
+		 */
+		final Button antwortenButton = new Button("Antwort absenden");
+		zurückButton.setStylePrimaryName("edit-button");
+		this.add(antwortenButton);
 		
 	}
 }
