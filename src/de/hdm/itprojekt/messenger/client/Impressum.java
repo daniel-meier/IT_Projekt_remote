c61aab75ef43696650b43875efd433eb73127bd1
ChristianReichardt <cr076@hdm-stuttgart.de>
2015-12-03 11:29:35 +0100

    Namen zum Impressum über Labels hinzugefügt

--- src/de/hdm/itprojekt/messenger/client/Impressum.java
+++ src/de/hdm/itprojekt/messenger/client/Impressum.java
@@ -13,6 +13,24 @@ public class Impressum extends VerticalPanel {
 		Label l = new Label("Impressum");
 		l.setStyleName("label1");
         this.add(l);
+        
+        Label Dani = new Label("Daniel Meier");
+        this.add(Dani);
+        
+        Label Kim = new Label("Kim Ishola");
+        this.add(Kim);
+        
+        Label Chris = new Label("Christian Reichardt");
+        this.add(Chris);
+        
+        Label Sarah = new Label("Sarah Weimer");
+        this.add(Sarah);
+        
+        Label Joey = new Label("Joel Siffermann");
+        this.add(Joey);
+        
+        Label Tino = new Label("Tino Hauler");
+        this.add(Tino);
          
         HorizontalPanel impressumPanel = new HorizontalPanel();
         RootPanel.get("editbuttons").add(impressumPanel);
