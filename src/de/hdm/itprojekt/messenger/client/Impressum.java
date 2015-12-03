af53b46683cb6b295599bd9a1b610e2f35389db3
ChristianReichardt <cr076@hdm-stuttgart.de>
2015-12-03 14:39:14 +0100

    Änderungen am Impressum vorgenommen

--- src/de/hdm/itprojekt/messenger/client/Impressum.java
+++ src/de/hdm/itprojekt/messenger/client/Impressum.java
@@ -10,14 +10,21 @@ public class Impressum extends VerticalPanel {
 	
 	public void onLoad() {
 		super.onLoad();
+		
+		HorizontalPanel impressumPanel = new HorizontalPanel();
+		RootPanel.get("impressum").add(impressumPanel);
+		
+		HorizontalPanel inhaltPanel = new HorizontalPanel();
+		RootPanel.get("inhalt").add(inhaltPanel);
+		
 		Label l = new Label("Impressum");
 		l.setStyleName("label1");
-        this.add(l);
+        impressumPanel.add(l);
         
         Label Dani = new Label("Daniel Meier");
-        this.add(Dani);
+        inhaltPanel.add(Dani);
         
-        Label Kim = new Label("Kim Ishola");
+        Label Kim = new Label("Name: Kim Ishola	 Kürzel:ki004	Email:ki004@hdm-stuttgart.de");
         this.add(Kim);
         
         Label Chris = new Label("Christian Reichardt");
@@ -31,9 +38,6 @@ public class Impressum extends VerticalPanel {
         
         Label Tino = new Label("Tino Hauler");
         this.add(Tino);
-         
-        HorizontalPanel impressumPanel = new HorizontalPanel();
-        RootPanel.get("editbuttons").add(impressumPanel);
         
 	}
 
