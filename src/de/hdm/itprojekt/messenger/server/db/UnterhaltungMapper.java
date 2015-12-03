e832319cdcda7a565ba8f0ffbf4cdf840bd06dfa
tinohauler <th098@hdm-stuttgart.de>
2015-12-03 18:37:11 +0100

    UnterhaltungMapper: Einfügen eines Unterhaltungs Objekts in die Datenbank

--- src/de/hdm/itprojekt/messenger/server/db/UnterhaltungMapper.java
+++ src/de/hdm/itprojekt/messenger/server/db/UnterhaltungMapper.java
@@ -31,7 +31,7 @@ public class UnterhaltungMapper extends DBConnection{
 			Statement stmt = con.createStatement();
 			
 			//Statement ausf¸llen und als Query an die B schicken
-			ResultSet rs = stmt.executeQuery("SELECT ID,  erstellungsdatum FROM unterhaltung "
+			ResultSet rs = stmt.executeQuery("SELECT ID,  Erstellungszeitpunkt FROM unterhaltung "
 					+ "WHERE ID=" + id + " ORDER BY Datum");
 			
 			/*
@@ -41,7 +41,7 @@ public class UnterhaltungMapper extends DBConnection{
 				//Ergebnis-Tupel in Objekt umwandeln
 				Unterhaltung u = new Unterhaltung();
 				u.setID(rs.getInt("ID"));
-				u.setErstellungdatum(rs.getDate("Erstellungsdatum"));
+				u.setErstellungszeitpunkt(rs.getDate("Erstellungszeitpunkt"));
 				return u;
 			}
 		}
@@ -60,8 +60,49 @@ public class UnterhaltungMapper extends DBConnection{
 	 * @return
 	 */
 	public UnterhaltungMapper einfuegen (unterhaltung Unterhaltung) {
+	Connection con = DBConnection.connection();
 		
+		try {
+			Statement stmt = con.createStatement();
+			
+			/*
+			 * Zun‰chst schauen wir nach, welches der mmentan hˆchste 
+			 * Prim‰rsch¸sselwert ist.
+			 */
+			ResultSet rs = stmt.executeQuery("SELECT MAX(id AS maxid"
+					+ "FROM unterhaltung ");
+			
+			// Wenn wir etwas zur¸ckerhalten, kann dies nur einzeilig sein
+			if (rs.next()) {
+				/*
+				 * t erh‰lt den bisher maximalen, un um 1 inkremetierten
+				 * Prim‰rschl¸ssel.
+				 */
+				Unterhaltung.setID(rs.getInt("maxid")+1);
+				
+			stmt = con.createStatement();
+			
+			//Jetzt erst erfolgt die tats‰chliche Einf¸geoperation
+			stmt.executeUpdate("INSERT INTO unterhaltung (unterhaltungsID, Erstellzeitpunkt" + "VALUES ("
+			+ Unterhaltung.getID() + "," + Unterhaltung.getErstellzeitpunkt() +"')");
+				
+				}
+			}
+			catch (SQLException e2) {
+				e2.printStackTrace();
+			}
+			/*
+			 * R¸ckgabe, des vtl. korrigierten Leistungsbringung.
+			 * 
+			 * HINWEIS: Da in Java nur Referenzen auf Objekte und keine physischen
+			 * Objekte ¸ergeben werden, w‰re die Anpassung des Textbeitrag-Objekts auch 
+			 * ohne diese esplizite R¸ckgabe auﬂerhalb dieser Methode sichtbar. Die
+			 * explizite R¸ckgabe von t ist eher ein Stilmittel, um zu signalisieren, 
+			 * dass sich das Objekt evtl. im Laufer der Methode ver‰ndert hat.
+			 */
+			return Unterhaltung;
 	}
+		
 	
 	/** Bearbeiten eines Unterhaltungs Objekts in der Datenbank
 	 * 
