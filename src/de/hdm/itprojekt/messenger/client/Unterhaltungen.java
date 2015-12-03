cb3dd66273756d468e9de862e4e6916a760b73a3
ChristianReichardt <cr076@hdm-stuttgart.de>
2015-12-03 14:33:10 +0100

    Auf Unterhaltung_Details Seite: löschen button und zurück button inklusive funktionalitäten (PopUp usw) angelegt.

--- src/de/hdm/itprojekt/messenger/client/Unterhaltungen.java
+++ src/de/hdm/itprojekt/messenger/client/Unterhaltungen.java
@@ -1,5 +1,18 @@
 package de.hdm.itprojekt.messenger.client;
 
-public class Unterhaltungen {
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.*;
 
+
+public class Unterhaltungen extends VerticalPanel {
+
+		public void onLoad(){
+		super.onLoad();
+		
+		Label l = new Label("Hier wird später ein FlexTable, dass die Unterhaltungen enthält, angezeigt");
+        this.add(l);
+		
+		
+		}
 }
