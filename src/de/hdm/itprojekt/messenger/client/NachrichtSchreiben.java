4e3e60610ac8535a49de58c42b94257a7d1a2aad
KimIshola <ki004@hdm-stuttgart.de>
2015-12-03 15:51:45 +0100

    TextArea, SuggestBoxen und Buttons inklusive ClickHandler angelegt

--- src/de/hdm/itprojekt/messenger/client/NachrichtSchreiben.java
+++ src/de/hdm/itprojekt/messenger/client/NachrichtSchreiben.java
@@ -1,24 +1,88 @@
 package de.hdm.itprojekt.messenger.client;
 
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.user.client.ui.*;
+import com.google.gwt.event.dom.client.ClickHandler;
 
 public class NachrichtSchreiben extends VerticalPanel{
 	
 	public void onLoad() {
 		super.onLoad();
-		RichTextArea text = new RichTextArea();
-		this.add(text);
 		
-		 HorizontalPanel buttonPanel = new HorizontalPanel();	
+		  HorizontalPanel buttonPanel = new HorizontalPanel();
 			RootPanel.get("editbuttons").add(buttonPanel);
-		    
-		    
+		
+			
+			/**
+			 * TextArea, um eine Nachricht zu schreiben
+			 */
+		 TextArea nachrichtSchreiben = new TextArea();
+		    nachrichtSchreiben.setCharacterWidth(80);
+		    nachrichtSchreiben.setVisibleLines(10);
+		    this.add(nachrichtSchreiben);
+		
+		        
+			/**
+			 * Oracle, dass die vorzuschlagenden Wörter der suggestBox enthält
+			 */
+			MultiWordSuggestOracle hinzufuegenOracle = new MultiWordSuggestOracle ();
+			hinzufuegenOracle.add("Meier");
+			hinzufuegenOracle.add("Meler");
+			
+			/**
+			 * SuggestBox, die anschließend dem Panel hingefügt wird
+			 */
+			final SuggestBox hinzufuegenSuggestBox = new SuggestBox(hinzufuegenOracle);
+			buttonPanel.add(hinzufuegenSuggestBox);
+			
 			/**
 			 * Button
 			 */
-			final Button findenButton = new Button("Finden");
-			findenButton.setStylePrimaryName("edit-button");
-			buttonPanel.add(findenButton);
-	}
+			Button hinzufuegenButton = new Button("Hinzufügen" , new ClickHandler() {
+				public void onClick(ClickEvent event) {
+				//Hier Applikationslogik für hinzufügen eines Empfängers eingüfen!!!
+					
+					
+				}
+			});
+	
+			buttonPanel.add(hinzufuegenButton);
+		
+			
+			/**
+			 * Oracle, dass die vorzuschlagenden Hashtags der suggestBox enthält
+			 */
+			MultiWordSuggestOracle hinzufuegenHashtagOracle = new MultiWordSuggestOracle ();
+			hinzufuegenOracle.add("#0711");
+			hinzufuegenOracle.add("#Stuttgart");
+			hinzufuegenOracle.add("#Hochschule der Medien");
+			hinzufuegenOracle.add("#Ich Liebe das IT-Projekt");
+			
+			/**
+			 * SuggestBox, die anschließend dem Panel hingefügt wird
+			 */
+			final SuggestBox hinzufuegenHashtagSuggestBox = new SuggestBox(hinzufuegenOracle);
+			buttonPanel.add(hinzufuegenHashtagSuggestBox);
+			
+			/**
+			 * Button
+			 */
+			Button hinzufuegenHashtagButton = new Button("Hinzufügen" , new ClickHandler() {
+				public void onClick(ClickEvent event) {
+				//Hier Applikationslogik für hinzufügen eines Hashtags eingüfen!!!
+					
+					
+				}
+			});		
+			buttonPanel.add(hinzufuegenHashtagButton);
+			
+			 /**
+			 * Button
+			 */
+			final Button sendenButton = new Button("Senden");
+			sendenButton.setStylePrimaryName("edit-button");
+			this.add(sendenButton);
+		
 
+}
 }
