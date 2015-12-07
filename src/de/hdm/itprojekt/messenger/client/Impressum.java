2f47690482ced82617bd624b83c2412c239c8977
ChristianReichardt <cr076@hdm-stuttgart.de>
2015-12-07 11:45:19 +0100

    Inhalte der Impressums angepasst

--- src/de/hdm/itprojekt/messenger/client/Impressum.java
+++ src/de/hdm/itprojekt/messenger/client/Impressum.java
@@ -1,9 +1,9 @@
 package de.hdm.itprojekt.messenger.client;
 
+import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class Impressum extends VerticalPanel {
@@ -11,33 +11,42 @@ public class Impressum extends VerticalPanel {
 	public void onLoad() {
 		super.onLoad();
 		
-		HorizontalPanel impressumPanel = new HorizontalPanel();
-		RootPanel.get("impressum").add(impressumPanel);
+		HorizontalPanel headlinePanel = new HorizontalPanel();
+		RootPanel.get("headline").add(headlinePanel);
 		
-		HorizontalPanel inhaltPanel = new HorizontalPanel();
-		RootPanel.get("inhalt").add(inhaltPanel);
 		
-		Label l = new Label("Impressum");
-		l.setStyleName("label1");
-        impressumPanel.add(l);
+//		Label headline = new Label("Impressum");
+//		headline.setStyleName("impressum-headline");
+//        headlinePanel.add(headline);
         
-        Label Dani = new Label("Daniel Meier");
-        inhaltPanel.add(Dani);
+        Label gesetz = new Label("Telemediengesetz");
+        this.add(gesetz);
         
-        Label Kim = new Label("Name: Kim Ishola	 Kürzel:ki004	Email:ki004@hdm-stuttgart.de");
-        this.add(Kim);
+        Grid impressumGrid = new Grid(7, 3);
+          
+        impressumGrid.setWidget(0, 0, new Label("Name"));
+        impressumGrid.setWidget(0, 1, new Label("Kontakt"));
+        impressumGrid.setWidget(0, 2, new Label("Abteilung"));
+        impressumGrid.setWidget(1, 0, new Label("Kim Ishola"));
+        impressumGrid.setWidget(1, 1, new Label("ki004@hdm-stuttgart.de"));
+        impressumGrid.setWidget(1, 2, new Label("GUI"));
+        impressumGrid.setWidget(2, 0, new Label("Daniel Meier"));
+        impressumGrid.setWidget(2, 1, new Label("dm077@hdm-stuttgart.de"));
+        impressumGrid.setWidget(2, 2, new Label("GUI"));
+        impressumGrid.setWidget(3, 0, new Label("Christian Reichardt"));
+        impressumGrid.setWidget(3, 1, new Label("cr076@hdm-stuttgart.de"));
+        impressumGrid.setWidget(3, 2, new Label("GUI"));
+        impressumGrid.setWidget(4, 0, new Label("Joel Siffermann"));
+        impressumGrid.setWidget(4, 1, new Label("js232@hdm-stuttgart.de"));
+        impressumGrid.setWidget(4, 2, new Label("Applikation"));
+        impressumGrid.setWidget(5, 0, new Label("Sarah Weimer"));
+        impressumGrid.setWidget(5, 1, new Label("sw155@hdm-stuttgart.de"));
+        impressumGrid.setWidget(5, 2, new Label("Applikation"));
+        impressumGrid.setWidget(6, 0, new Label("Tino Hauler"));
+        impressumGrid.setWidget(6, 1, new Label("th098@hdm-stuttgart.de"));
+        impressumGrid.setWidget(6, 2, new Label("Datenbank"));
         
-        Label Chris = new Label("Christian Reichardt");
-        this.add(Chris);
-        
-        Label Sarah = new Label("Sarah Weimer");
-        this.add(Sarah);
-        
-        Label Joey = new Label("Joel Siffermann");
-        this.add(Joey);
-        
-        Label Tino = new Label("Tino Hauler");
-        this.add(Tino);
+        this.add(impressumGrid);
         
 	}
 
