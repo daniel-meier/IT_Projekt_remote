4462989231d94807042d64a403cc06dddb68bf7a
KimIshola <ki004@hdm-stuttgart.de>
2015-12-23 22:22:39 +0100

    Kommentare hinzugefügt (was noch gemacht werden muss)

--- src/de/hdm/itprojekt/messenger/client/report/AbonnementInfo.java
+++ src/de/hdm/itprojekt/messenger/client/report/AbonnementInfo.java
@@ -2,4 +2,9 @@ package de.hdm.itprojekt.messenger.client.report;
 
 public class AbonnementInfo extends ReportFormular{
 
+	/**
+	 * Hier muss die FlexTable für die beiden Abos rein!!!
+	 */
+	
 }
+ 
--- src/de/hdm/itprojekt/messenger/client/report/NachrichtenInfo.java
+++ src/de/hdm/itprojekt/messenger/client/report/NachrichtenInfo.java
@@ -1,5 +1,9 @@
 package de.hdm.itprojekt.messenger.client.report;
 
 public class NachrichtenInfo extends ReportFormular{
+	
+	/**
+	 * Hier muss die FlexTable für die NachrichtenInfos rein!!!
+	 */
 
 }
