ffd849cc1d4feaf2c5c34894b5a0972f49c5a19a
KimIshola <ki004@hdm-stuttgart.de>
2015-12-23 22:27:14 +0100

    Headline gesetzt

--- src/de/hdm/itprojekt/messenger/client/report/AbonnementInfo.java
+++ src/de/hdm/itprojekt/messenger/client/report/AbonnementInfo.java
@@ -2,9 +2,20 @@ package de.hdm.itprojekt.messenger.client.report;
 
 public class AbonnementInfo extends ReportFormular{
 
+	public void onLoad() {
+		super.onLoad();
+		
+		/**
+		 * Überschrift der Seite
+		 */
+		setHeadline("Informationen über Abonnements");
+		
+		
+		
 	/**
 	 * Hier muss die FlexTable für die beiden Abos rein!!!
 	 */
 	
+}
 }
  
--- src/de/hdm/itprojekt/messenger/client/report/NachrichtenInfo.java
+++ src/de/hdm/itprojekt/messenger/client/report/NachrichtenInfo.java
@@ -2,8 +2,17 @@ package de.hdm.itprojekt.messenger.client.report;
 
 public class NachrichtenInfo extends ReportFormular{
 	
+	public void onLoad() {
+		super.onLoad();
+		
+		/**
+		 * Überschrift der Seite
+		 */
+		setHeadline("Information über Nachricht");
+	
 	/**
 	 * Hier muss die FlexTable für die NachrichtenInfos rein!!!
 	 */
 
+}
 }
