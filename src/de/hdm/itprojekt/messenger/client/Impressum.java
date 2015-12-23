19ce49977d1fffec6e111e87e4ca6f2f5b77e049
ChristianReichardt <cr076@hdm-stuttgart.de>
2015-12-23 13:46:00 +0100

    Kommentare hinzugefügt

--- src/de/hdm/itprojekt/messenger/client/Impressum.java
+++ src/de/hdm/itprojekt/messenger/client/Impressum.java
@@ -8,15 +8,28 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class Impressum extends Formular {
 	
+	
 	public void onLoad() {
 		super.onLoad();
-		
+/**
+ * Überschrift der Seite		
+ */
 		setHeadline("Impressum");
 
-        VerticalPanel impressumPanel = new VerticalPanel();
+       
+/**
+ * Vertikales Panel für die Inhalte des Impressums		
+ */
+		VerticalPanel impressumPanel = new VerticalPanel();
         this.add(impressumPanel);
 		
-		Label gesetz = new Label("Angaben gemäß § 5 Telemediengesetz (TMG)");
+		
+/**
+ * Nachfolgend werden die Labels für die Anschrift der Hochschule definiert. 
+ * Durch die Verwendung der Labels ist er sehr einfach möglich die Zeilen untereinander
+ * stehen zu haben.        
+ */
+        Label gesetz = new Label("Angaben gemäß § 5 Telemediengesetz (TMG)");
 		gesetz.setStylePrimaryName("labelGesetz");
 		impressumPanel.add(gesetz);      
 		
@@ -30,9 +43,20 @@ public class Impressum extends Formular {
         Label plz = new Label("70569 Stuttgart");
         impressumPanel.add(plz);
         
+       
+/**
+ * Hier wird eine Tabelle als Grid definiert. Diese Art der Tabelle lässt sich nicht 
+ * flexibel erweitern. Dies ist in diesem Fall jedoch auch nicht notwendig. Sie 
+ * benötigt 7 Zeilen und 3 Spalten.        
+ */
         Grid impressumGrid = new Grid(7, 3);
         impressumGrid.setStyleName("impressumGrid");
         
+/**
+ * Hier werden die Inhalte in Labels für den Tabellenkopf definiert. Außerdem werden 
+ * zwei Labels für einen Namen und eine E-Mail definiert um diese in der CSS Datei
+ * ansprechen zu können.         
+ */
         Label name = new Label("NAME");
         name.setStyleName("labelName");
         impressumPanel.add(name);
@@ -52,7 +76,10 @@ public class Impressum extends Formular {
         Label eMailDani = new Label ("dm077@hdm-stuttgart.de");
         eMailDani.setStyleName("labelDani");
         impressumPanel.add(eMailDani);
-          
+        
+/**
+ * Hier werden der Tabelle die Inhalte per Labels direkt hinzugefügt.          
+ */
         impressumGrid.setWidget(0, 0, name);
         impressumGrid.setWidget(0, 1, kontakt);
         impressumGrid.setWidget(0, 2, abteilung);
@@ -75,6 +102,10 @@ public class Impressum extends Formular {
         impressumGrid.setWidget(6, 1, new Label("th098@hdm-stuttgart.de"));
         impressumGrid.setWidget(6, 2, new Label("Datenbank"));
         
+/**
+ * Hier wird dem Impressums Panel, welches am Anfang der Seite erstellt wurde, 
+ * die Grid Tabelle hinzugefügt.        
+ */
         impressumPanel.add(impressumGrid);
         
 	}
